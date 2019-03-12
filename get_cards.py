0927e6838e855e38032389fedb58c52a7c004450
pawloxx <murinus.dot@gmail.com>
2019-03-12 14:46:53 +0100

    added comments, new vars

--- get_cards.py
+++ get_cards.py
@@ -1,20 +1,38 @@
 import os
 from trello import TrelloClient
 
-api_key = os.environ['API_KEY']
-api_secret = os.environ['API_SECRET']
+# variables
+api_k = os.environ['api_key']
+api_s = os.environ['api_secret']
+board_name_track = 'Board Trello redmine integration' # variable with name of Board to track
 
-client = TrelloClient(api_key=api_key, api_secret=api_secret)
+# API access
+client = TrelloClient(api_key=api_k, api_secret=api_s)
 
+# board access
 all_boards = client.list_boards()
-my_board = all_boards[2]
+my_board = all_boards[2]  # need to find solution for that!
 my_lists = my_board.list_lists()
 
+
+print(all_boards)
+print(my_board)
 print(my_lists)
 
-def get_todo_item():
+
+def get_cards():
     to_do = []
     for card in my_lists:
         cards = card.list_cards()
+        if card.closed == False:
+            for i in cards:
+                to_do.append(i.name)
+    return to_do
 
-print(get_todo_item())
+print(get_cards())
+
+
+#nazwy card
+#opisy card
+#userzy card
+

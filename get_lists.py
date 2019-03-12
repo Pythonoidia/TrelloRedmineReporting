e2cc1609378d0866ef012740bf4e145ba5458813
pawloxx <murinus.dot@gmail.com>
2019-03-12 11:39:27 +0100

    added en variables

--- get_lists.py
+++ get_lists.py
@@ -1,9 +1,20 @@
+import os
 from trello import TrelloClient
 
-client = TrelloClient(api_key='241061d740372ffdb4ad7902ae908261', api_secret='8a58e6ab452564a9539c13fd546e59b728c073de4d0b7a23dc2793326c96f5f2')
+api_key = os.environ['API_KEY']
+api_secret = os.environ['API_SECRET']
+
+client = TrelloClient(api_key=api_key, api_secret=api_secret)
 
 all_boards = client.list_boards()
 my_board = all_boards[2]
 my_lists = my_board.list_lists()
 
 print(my_lists)
+
+def get_todo_item():
+    to_do = []
+    for card in my_lists:
+        cards = card.list_cards()
+
+print(get_todo_item())

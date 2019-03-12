f071a47a2db1ca94c57202bcb2d38f62e8281257
krzysztofsmokowski <firk@vps657107.localdomain>
2019-03-12 20:01:28 +0100

    added headers

--- redmine.py
+++ redmine.py
@@ -6,7 +6,7 @@ class RedmineProjects(object):
     def __init__(self):
        self.redmine_login = os.environ["hg_redmine"]
        self.redmine_password = os.environ["redmine_pass"]
-       self.json_data = {"issue":{"project_id":1, "tracker_id":1, "status_id":1, "priority_id":1, "subject": "Examplepost", "priority_id": 4}}
+       self.json_data = {"issue":{"project_id":1, "tracker_id":1, "status_id":1, "priority_id":1, "subject": "Examplepost"}}
        #self.jsoned = {"issue":{"project_id":1, "tracker_id":1}}
        self.hg = os.environ["hg_url"]
 
@@ -17,7 +17,8 @@ class RedmineProjects(object):
         return requests.get(self.hg.format("issues.json")).json()
 
     def redmine_post(self):
-        return requests.post(url=self.hg.format("issues.json"), data=self.json_data, auth=(self.redmine_login, self.redmine_password))
+        headers = {'Content-type': 'application/json'}
+        return requests.post(url=self.hg.format("issues.json"), data=self.json_data, headers=headers, auth=(self.redmine_login, self.redmine_password))
 
         
 
@@ -25,7 +26,7 @@ class RedmineProjects(object):
 def main():
     RD = RedmineProjects()
     print(RD.redmine_post())
-    print(RD.redmine_get_issues())
+    #print(RD.redmine_get_issues())
 
 if __name__ == '__main__':
     main()
